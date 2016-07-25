c44263f189b1eef927bc4b5aca00f49434c2245d
Kai Kewley <kaikewley@gmail.com>
2016-07-25 15:45:58 +0100

    Relationship counting moved to drools rule, remove helper method.

--- src/main/java/org/ihtsdo/drools/helper/RelationshipHelper.java
+++ src/main/java/org/ihtsdo/drools/helper/RelationshipHelper.java
@@ -9,17 +9,6 @@ import java.util.Set;
 
 public class RelationshipHelper {
 
-	public static int countRelationshipsInGroup(Concept concept, int group) {
-		int count = 0;
-		for (Relationship relationship : concept.getRelationships()) {
-			if (!Constants.INFERRED_RELATIONSHIP.equals(relationship.getCharacteristicTypeId())
-					&& relationship.getRelationshipGroup() == group) {
-				count++;
-			}
-		}
-		return count;
-	}
-
 	public static boolean hasActiveRelationshipOfType(Concept concept, String typeId) {
 		for (Relationship relationship : concept.getRelationships()) {
 			if (relationship.isActive() && typeId.equals(relationship.getTypeId())) {
