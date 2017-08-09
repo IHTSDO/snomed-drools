5a7b6ac2cf9baad3c477769ab59ae8eeacaf817f
Kai Kewley <kaikewley@gmail.com>
2017-08-09 14:55:07 +0100

    APDS-288 Add placeholders for new ConceptService methods in RF2 implementation.

--- snomed-drools-rf2-validator/src/main/java/org/ihtsdo/drools/validator/rf2/service/DroolsConceptService.java
+++ snomed-drools-rf2-validator/src/main/java/org/ihtsdo/drools/validator/rf2/service/DroolsConceptService.java
@@ -1,7 +1,11 @@
 package org.ihtsdo.drools.validator.rf2.service;
 
+import org.ihtsdo.drools.domain.Concept;
 import org.ihtsdo.drools.service.ConceptService;
 import org.ihtsdo.drools.validator.rf2.SnomedDroolsComponentRepository;
+
+import java.util.Collections;
+import java.util.Set;
 
 public class DroolsConceptService implements ConceptService {
 
@@ -14,5 +18,23 @@ public class DroolsConceptService implements ConceptService {
 	@Override
 	public boolean isActive(String conceptId) {
 		return repository.getConcept(conceptId).isActive();
+	}
+
+	@Override
+	public Set<Concept> getAllTopLevelHierachies() {
+		// TODO: Add support for this
+		return Collections.emptySet();
+	}
+
+	@Override
+	public Set<Concept> findAllStatedAncestorsOfConcept(Concept concept) {
+		// TODO: Add support for this
+		return Collections.emptySet();
+	}
+
+	@Override
+	public Set<Concept> findAllTopLevelHierachiesOfConcept(Concept concept) {
+		// TODO: Add support for this
+		return Collections.emptySet();
 	}
 }
