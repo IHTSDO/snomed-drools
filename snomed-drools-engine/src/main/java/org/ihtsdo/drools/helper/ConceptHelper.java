882e4ec0a709732cc1d7634676e5bac982f2ae09
HMVNLTP025\ThaiHuynh <thai.huynh@huntermacdonald.com>
2017-10-03 17:40:23 +0700

    APDS-288 add debug log

--- snomed-drools-engine/src/main/java/org/ihtsdo/drools/helper/ConceptHelper.java
+++ snomed-drools-engine/src/main/java/org/ihtsdo/drools/helper/ConceptHelper.java
@@ -19,7 +19,9 @@ public class ConceptHelper {
 	public static boolean isMoreThanOneTopLevelHierarchy(List<Concept> concepts, String... restrictConcepts){
 		List<String> sourceId = new ArrayList<String>();
 		for(Concept c : concepts){
-			sourceId.add(c.getId());
+			if  (!sourceId.contains(c.getId())) {
+				sourceId.add(c.getId());
+			}
 		}
 		return (sourceId.size() > 1);
 	}
