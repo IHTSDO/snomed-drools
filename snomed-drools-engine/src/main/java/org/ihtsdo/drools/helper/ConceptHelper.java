1e2face494d037cefee9a0b2b9b01cd451071fe4
Quyen Ly <quyen.ly@huntermacdonald.com>
2018-03-16 10:14:44 +0700

    APDS-288 Drools Rules [new]: Concept parents in more than one top-level hierarchy - correct Helper class

--- snomed-drools-engine/src/main/java/org/ihtsdo/drools/helper/ConceptHelper.java
+++ snomed-drools-engine/src/main/java/org/ihtsdo/drools/helper/ConceptHelper.java
@@ -4,42 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.ihtsdo.drools.domain.Concept;
-
 public class ConceptHelper {
 	
-	public static boolean containsAnyConcept(List<Concept> concepts, String... restrictConcepts){
-		List<String> topLevelConceptIds = collectConceptId(concepts);
-		List<String> restrictList = new ArrayList<String>(Arrays.asList(restrictConcepts));
-		
-		return topLevelConceptIds.size() > 1 && topLevelConceptIds.size() == restrictList.size() && topLevelConceptIds.containsAll(restrictList);
+	public static boolean containsAnyConcept(List<String> conceptIdList, String... restrictConcepts){
+		List<String> restrictList = new ArrayList<String>(Arrays.asList(restrictConcepts));		
+		return conceptIdList.size() > 1 && conceptIdList.size() == restrictList.size() && conceptIdList.containsAll(restrictList);
 	}
 	
-	public static boolean isMoreThanOneTopLevelHierarchy(List<Concept> concepts, String... restrictConcepts){
-		List<String> topLevelConceptIds = collectConceptId(concepts);
+	public static boolean isMoreThanOneTopLevelHierarchy(List<String> conceptIdList, String... restrictConcepts){
 		List<String> restrictList = new ArrayList<String>(Arrays.asList(restrictConcepts));
 		
-		if (topLevelConceptIds.size() > 1) {
-			if (topLevelConceptIds.size() > restrictList.size()) {
+		if (conceptIdList.size() > 1) {
+			if (conceptIdList.size() > restrictList.size()) {
 				return true;
-			} else if (topLevelConceptIds.size() == restrictList.size()) {
-				return !topLevelConceptIds.containsAll(restrictList);
+			} else if (conceptIdList.size() == restrictList.size()) {
+				return !conceptIdList.containsAll(restrictList);
 			} else {
 				return false;
 			}
 		}
 		
 		return false;
-	}
-	
-	private static List<String> collectConceptId(List<Concept> concepts) {
-		List<String> conceptIds = new ArrayList<String>();
-		for(Concept c : concepts){
-			if (!conceptIds.contains(c.getId())) {
-				conceptIds.add(c.getId());
-			}
-		}
-		
-		return conceptIds;
 	}
 }
