bf782ff1043070f73d2ca47f12fd4be7327719d5
Kai Kewley <kaikewley@gmail.com>
2018-11-27 15:11:17 +0000

    MAINT-153 Add composite id for relationships generated from axioms.

--- snomed-drools-engine/src/main/java/org/ihtsdo/drools/domain/Relationship.java
+++ snomed-drools-engine/src/main/java/org/ihtsdo/drools/domain/Relationship.java
@@ -1,5 +1,8 @@
 package org.ihtsdo.drools.domain;
 
+/**
+ * Represents a stated relationship, OWL Axiom fragment or inferred relationship
+ */
 public interface Relationship extends Component {
 
 	String getSourceId();
